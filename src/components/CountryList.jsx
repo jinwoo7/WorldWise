27d5535216d54169885a58d3387b20883f0e795e
jinwoo <jinwoo7@x1.attlocal.net>
2024-04-06 08:46:16 -0700

    adding custom hook and adding back button

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,8 +2,11 @@ import styles from "./CountryList.module.css";
 import Message from "./Message";
 import Spinner from "./Spinner";
 import CountryItem from "./CountryItem";
+import { useCities } from "../contexts/CitiesContext";
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
